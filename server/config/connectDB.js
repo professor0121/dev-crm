f1a31cf6ab260e7b0bbad7a0c210db5796bc8c0d
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-05-04 10:25:29 +0530

    project in the process

---
+++ server/config/connectDB.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+
+console.log(MONGO_URI);
+const connectDB = async () => {
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+    try {
+        await mongoose.connect(MONGO_URI, options)
+        console.log("MongoDB connected successfully")
+    } catch (err) {
+        console.log(err.message)
+        process.exit(1)
+    }
+}
+module.exports = connectDB;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/router');
+const connectDB = require('./config/connectDB');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT;
+
+connectDB();
+
+
+app.use("/", router)
+app.listen(PORT, () => {
+  console.log(`the server is running on the port ${PORT}`)
+}) 
